f80b53d9f93aebd080a2101ab5c0820a7a3fc036
Simmarum <mateusz0831@gmail.com>
2021-11-17 00:25:48 +0100

    Add TOC to stats

--- stats/stats.sh
+++ stats/stats.sh
@@ -2,15 +2,18 @@
 
 set -e
 
-template='README.md.template'
 readme='README.md'
-stats_match='sed-day-stats'
 
-cp $template $readme
+content="# Advent of Code\n\nhttps://adventofcode.com/\n\n# Table of contents\n"
+content_toc=""
+content_stats=""
 
 for yeardir in ../20*; do
     year=${yeardir: -4}
     echo $yeardir
+    content_toc=$content_toc"\n[$year](#$year)"
+    content_stats=$content_stats"\n## $year\nYear | Day | LOC | \`flake8\` | \`for\` | \`while\`\n--- | --- | --- | --- | --- | ---\n"
+
     for dir in $yeardir/d*; do
         echo $dir
         main=$dir/main.py
@@ -19,9 +22,8 @@ for yeardir in ../20*; do
         fors=$(grep -E '(\s|^)for(\s|$)' $main | wc -l)
         whiles=$(grep -E '(\s|^)while(\s|$)' $main | wc -l)
         day_num=$(echo ${dir: -2} | bc)
-        line="$year | $day_num | $loc | $flake | $fors | $whiles"
-        gsed -i "/$stats_match/i $line" $readme
+        content_stats=$content_stats"$year | $day_num | $loc | $flake | $fors | $whiles\n"
     done
 done
 
-gsed -i "s/$stats_match//g" $readme
+echo -e $content$content_toc"\n"$content_stats > $readme
