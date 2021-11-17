4eae5ff032e74dd0b0c526e0ecab66ba3b609076
Simmarum <mateusz0831@gmail.com>
2021-11-17 10:48:47 +0100

    2015-day-15

---
+++ 2015/day-15/main.py
@@ -0,0 +1,60 @@
+import itertools
+import math
+
+
+def read_file() -> list:
+    with open(f"{__file__.rstrip('main.py')}input.txt", "r") as f:
+        return [str(line).replace("\n", "") for line in f.readlines()]
+
+
+def part_1(inp):
+    ings = []
+    for one_line in inp:
+        split_line = one_line.split(" ")
+        ings.append([
+            int(split_line[2][:-1]),
+            int(split_line[4][:-1]),
+            int(split_line[6][:-1]),
+            int(split_line[8][:-1])
+        ])
+    ings_inv = list(map(list, zip(*ings)))
+
+    all_combs = [x for x in itertools.permutations(
+        range(0, 101), len(ings)) if sum(x) == 100]
+    res = max([math.prod([max(0, sum([a * b for a, b in zip(one_comb, one_ing_inv)]))
+                          for one_ing_inv in ings_inv]) for one_comb in all_combs])
+    return res
+
+
+def part_2(inp):
+    ings = []
+    for one_line in inp:
+        split_line = one_line.split(" ")
+        ings.append([
+            int(split_line[2][:-1]),
+            int(split_line[4][:-1]),
+            int(split_line[6][:-1]),
+            int(split_line[8][:-1]),
+            int(split_line[10])
+        ])
+    ings_inv = list(map(list, zip(*ings)))
+    lings = len(ings)
+
+    all_combs = [x for x in itertools.permutations(
+        range(0, 101), lings) if sum(x) == 100]
+    res = max([math.prod([max(0, sum([a * b for a, b in zip(one_comb, one_ing_inv)]))
+                          for one_ing_inv in ings_inv[:-1]]) for one_comb in all_combs
+               if sum([a * b for a, b in zip(one_comb, ings_inv[-1])]) == 500])
+    return res
+
+
+def main():
+    inp = read_file()
+    res_1 = part_1(inp)
+    print(f"res_1: {res_1}")
+    res_2 = part_2(inp)
+    print(f"res_2: {res_2}")
+
+
+if __name__ == '__main__':
+    main()
