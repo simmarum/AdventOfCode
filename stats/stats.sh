8def0089b970ead02e8607c684321039209b8e60
Simmarum <mateusz0831@gmail.com>
2023-12-16 16:29:46 +0100

    Update stats

--- stats/stats.sh
+++ stats/stats.sh
@@ -8,22 +8,33 @@ content="# Advent of Code\n\nhttps://adventofcode.com/\n\n# Table of contents\n"
 content_toc=""
 content_stats=""
 
+get_stat() {
+    dir=$1
+    year=$2
+    day_num=$(echo ${dir: -2} | bc)
+    main=$dir/main.py
+    loc=$(wc -l <$main)
+    flake=$(flake8 --max-line-length=99 $main | wc -l)
+    fors=$(grep -E '(\s|^)for(\s|$)' $main | wc -l)
+    whiles=$(grep -E '(\s|^)while(\s|$)' $main | wc -l)
+
+    mkdir -p tmp/
+    echo "$year | $day_num | $loc | $flake | $fors | $whiles" >>tmp/$year.stats
+}
 for yeardir in ../20*; do
     year=${yeardir: -4}
     echo $yeardir
     content_toc=$content_toc"\n[$year](#$year)"
     content_stats=$content_stats"\n## $year\nYear | Day | LOC | \`flake8\` | \`for\` | \`while\`\n--- | --- | --- | --- | --- | ---\n"
 
+    rm -rf tmp/$year.stats
     for dir in $yeardir/d*; do
-        echo $dir
-        main=$dir/main.py
-        loc=$(wc -l < $main)
-        flake=$(flake8 --max-line-length=99 $main | wc -l)
-        fors=$(grep -E '(\s|^)for(\s|$)' $main | wc -l)
-        whiles=$(grep -E '(\s|^)while(\s|$)' $main | wc -l)
-        day_num=$(echo ${dir: -2} | bc)
-        content_stats=$content_stats"$year | $day_num | $loc | $flake | $fors | $whiles\n"
+        get_stat $dir $year &
     done
+    wait
+
+    content_stats="$content_stats""$(cat tmp/$year.stats | sort -V)"
+
 done
 
-echo -e $content$content_toc"\n"$content_stats > $readme
+echo -e $content$content_toc"\n""$content_stats" >$readme
